853965f84ec5d9644fc901c4f6c066f1b669e45e
NisanthDilip <nisanthanela1999@gmail.com>
2025-07-31 13:19:34 +0900

    プロジェクト ファイルを追加します。

---
+++ methods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            int result=mymethod();
+            Console.WriteLine(result);
+            
+        }
+        public static int mymethod()
+        {
+            Console.WriteLine("Enter A Value");
+            int a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter A Value");
+            int b = Convert.ToInt32(Console.ReadLine());
+            return a + b;
+
+        }
+
+
+    }
+}
